239cab8d4d39b5d84ab3edb8e4845506e3a04f7f
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-07-01 11:28:11 +1000

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,8 @@ CODESPACE_NAME = os.getenv(key="CODESPACE_NAME", default="blank")
 
 if CODESPACE_NAME.startswith("dttest-"):
 
-    send_startup_ping(codespace_mode="testing")
-    # TODO
+    # Do your testing logic here.
     # Testing done, force kill codespace
     subprocess.run(["gh", "codespace", "delete", "--codespace", CODESPACE_NAME, "--force"])
 else:
-    send_startup_ping(codespace_mode="standard")
+    # This is a "real" startup, so do not test and proceed as normal
